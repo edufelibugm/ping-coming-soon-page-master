7582dfeac21d7671b47983ccd57d7c56419eab4d
edufelibugm <106450903+edufelibugm@users.noreply.github.com>
2022-09-16 17:39:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.btn').addEventListener('click', () => {
+    document.getElementById('mail-cont').classList.add('error-detect');
+    document.querySelector('.error-mnsg').classList.add('error-detect');
+})
